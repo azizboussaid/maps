6d0697a072aa7761794ade72abc8c0664f44eccd
aziz boussaid <azizboussaied062@gmail.com>
2023-12-16 17:18:23 +0100

    maps

---
+++ maps/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MapContainer from './MapContainer';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Google Map</h1>
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
+
